383fe0bc6613f59d94723945fcbe441d9a695451
Arno Haase <arno.haase@haase-consulting.com>
2024-10-21 13:07:12 +0200

    serialize / deserialize NodeAddr

--- src/messaging/node_addr.rs
+++ src/messaging/node_addr.rs
@@ -1,8 +1,11 @@
 use std::fmt::{Debug, Formatter};
-use std::net::SocketAddr;
+use std::net::{SocketAddr, SocketAddrV4, SocketAddrV6};
 use std::time::{SystemTime, UNIX_EPOCH};
+use anyhow::anyhow;
 
 use bytes::{Buf, BufMut};
+
+use bytes_varint::try_get_fixed::TryGetFixedSupport;
 
 /// Nodes' lifecycle of membership in a _cluster is monotonous to allow tracking with CRDTs, so a
 ///  node can never rejoin once it left (or was evicted). To allow rejoining from the same network
@@ -35,12 +38,46 @@ impl NodeAddr {
         }
     }
 
+    //TODO unit test
     pub fn ser(&self, buf: &mut impl BufMut) {
-        todo!()
+        buf.put_u32(self.unique);
+        match &self.addr {
+            SocketAddr::V4(data) => {
+                buf.put_u8(4);
+                buf.put_u32(data.ip().to_bits());
+                buf.put_u16(data.port());
+            }
+            SocketAddr::V6(data) => {
+                buf.put_u8(6);
+                buf.put_u128(data.ip().to_bits());
+                buf.put_u16(data.port());
+            }
+        }
     }
 
+    //TODO unit test
     pub fn try_deser(buf: &mut impl Buf) -> anyhow::Result<NodeAddr> {
-        todo!()
+        let unique = buf.try_get_u32()?;
+
+        let addr = match buf.try_get_u8()? {
+            4 => {
+                let ip = buf.try_get_u32()?;
+                let port = buf.try_get_u16()?;
+                SocketAddr::V4(SocketAddrV4::new(ip.into(), port))
+            }
+            6 => {
+                let ip = buf.try_get_u128()?;
+                let port = buf.try_get_u16()?;
+                SocketAddr::V6(SocketAddrV6::new(ip.into(), port, 0, 0))
+            }
+            n => {
+                return Err(anyhow!("invalid socket address discrimiator: {}", n));
+            }
+        };
+        Ok(NodeAddr {
+            unique,
+            addr,
+        })
     }
 }
 
