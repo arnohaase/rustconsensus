f5eeb6270473b73b701773aa60aabd96b8df82c8
Arno Haase <arno.haase@haase-consulting.com>
2025-02-04 12:08:20 +0100

    unit tests for control messages

--- messaging/src/control_messages.rs
+++ messaging/src/control_messages.rs
@@ -1,13 +1,11 @@
+use std::fmt::{Debug, Formatter};
 use bytes::{Buf, BufMut, BytesMut};
 use crate::packet_id::PacketId;
 use bytes_varint::try_get_fixed::TryGetFixedSupport;
 use bytes_varint::{VarIntSupport, VarIntSupportMut};
 
-//TODO human readable Debug
-#[derive(Debug)]
+#[derive(Clone, Eq, PartialEq)]
 pub struct ControlMessageRecvSync {
-    //TODO update specification to reflect this
-
     /// The id *after* the highest received packet (to allow for 0 initially)
     pub receive_buffer_high_water_mark: PacketId,
     /// The id *after* the lowest packet that was not fully dispatched yet.
@@ -17,8 +15,18 @@ pub struct ControlMessageRecvSync {
     /// NB: This can be higher than the 'low-water mark' for multi-packet messages
     pub receive_buffer_ack_threshold: PacketId,
 }
+impl Debug for ControlMessageRecvSync {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "RECV_SYNC ({}/{}/{})",
+            self.receive_buffer_low_water_mark.to_raw(),
+            self.receive_buffer_ack_threshold.to_raw(),
+            self.receive_buffer_high_water_mark.to_raw(),
+        )
+    }
+}
 impl ControlMessageRecvSync {
-    //TODO unit test ser / deser
     pub fn ser(&self, buf: &mut BytesMut) {
         buf.put_u64(self.receive_buffer_high_water_mark.to_raw());
         buf.put_u64(self.receive_buffer_low_water_mark.to_raw());
@@ -37,16 +45,24 @@ impl ControlMessageRecvSync {
     }
 }
 
-//TODO human readable Debug
-#[derive(Debug)]
+#[derive(Clone, Eq, PartialEq)]
 pub struct ControlMessageSendSync {
     /// The id *after* the highest sent packet
     pub send_buffer_high_water_mark: PacketId,
     /// The id *after* the oldest packet still in the send buffer
     pub send_buffer_low_water_mark: PacketId,
 }
+impl Debug for ControlMessageSendSync {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "SEND_SYNC ({}/{})",
+            self.send_buffer_low_water_mark.to_raw(),
+            self.send_buffer_high_water_mark.to_raw(),
+        )
+    }
+}
 impl ControlMessageSendSync {
-    //TODO unit test ser / deser
     pub fn ser(&self, buf: &mut BytesMut) {
         buf.put_u64(self.send_buffer_high_water_mark.to_raw());
         buf.put_u64(self.send_buffer_low_water_mark.to_raw());
@@ -62,13 +78,20 @@ impl ControlMessageSendSync {
     }
 }
 
-#[derive(Debug)] //TODO human readable Debug
+#[derive(Clone, Eq, PartialEq)]
 pub struct ControlMessageNak {
     pub packet_id_resend_set: Vec<PacketId>,
 }
-
+impl Debug for ControlMessageNak {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "NAK {:?}", self.packet_id_resend_set
+            .iter()
+            .map(|id| id.to_raw())
+            .collect::<Vec<_>>()
+        )
+    }
+}
 impl ControlMessageNak {
-    //TODO unit test ser / deser
     fn ser(&self, buf: &mut BytesMut) {
         buf.put_usize_varint(self.packet_id_resend_set.len());
         for &packet_id in &self.packet_id_resend_set {
@@ -83,5 +106,69 @@ impl ControlMessageNak {
             packet_id_resend_set.push(PacketId::from_raw(buf.try_get_u64()?));
         }
         Ok(ControlMessageNak { packet_id_resend_set })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(0, 0, 0)]
+    #[case(0, 1, 2)]
+    #[case(1231231231, 1231231236, 1231231239)]
+    #[case(u64::MAX, u64::MAX, u64::MAX)]
+    fn test_ser_recv_sync(#[case] low_water_mark: u64, #[case] ack_threshold: u64, #[case] high_water_mark: u64) {
+        let original = ControlMessageRecvSync {
+            receive_buffer_low_water_mark: PacketId::from_raw(low_water_mark),
+            receive_buffer_ack_threshold: PacketId::from_raw(ack_threshold),
+            receive_buffer_high_water_mark: (PacketId::from_raw(high_water_mark)),
+        };
+
+        let mut buf = BytesMut::new();
+        original.ser(&mut buf);
+        let deser = ControlMessageRecvSync::deser(&mut buf).unwrap();
+
+        assert_eq!(deser, original);
+    }
+
+    #[rstest]
+    #[case(0, 0)]
+    #[case(1, 2)]
+    #[case(1231231231231231, 1231231231231239)]
+    #[case(u64::MAX, u64::MAX)]
+    fn test_ser_send_sync(#[case] low_water_mark: u64, #[case] high_water_mark: u64) {
+        let original = ControlMessageSendSync {
+            send_buffer_low_water_mark: PacketId::from_raw(low_water_mark),
+            send_buffer_high_water_mark: PacketId::from_raw(high_water_mark),
+        };
+
+        let mut buf = BytesMut::new();
+        original.ser(&mut buf);
+        let deser = ControlMessageSendSync::deser(&mut buf).unwrap();
+
+        assert_eq!(deser, original);
+    }
+
+    #[rstest]
+    #[case(vec![])]
+    #[case(vec![0])]
+    #[case(vec![1])]
+    #[case(vec![u64::MAX])]
+    #[case(vec![1, 2])]
+    #[case(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])]
+    fn test_ser_nak(#[case] ids: Vec<u64>) {
+        let ids = ids.into_iter().map(|id| PacketId::from_raw(id)).collect();
+        let original = ControlMessageNak {
+            packet_id_resend_set: ids,
+        };
+
+        let mut buf = BytesMut::new();
+        original.ser(&mut buf);
+        let deser = ControlMessageNak::deser(&mut buf).unwrap();
+
+        assert_eq!(deser, original);
     }
 }
