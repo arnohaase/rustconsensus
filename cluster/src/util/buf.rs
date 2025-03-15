f864000933e05ea0566f0fb7ac077fa4173d3dc8
Arno Haase <arno.haase@haase-consulting.com>
2025-03-15 17:52:17 +0100

    unit tests for put_string / try_get_string

--- cluster/src/util/buf.rs
+++ cluster/src/util/buf.rs
@@ -2,10 +2,7 @@ use bytes::{Buf, BufMut, BytesMut};
 use bytes_varint::{VarIntSupport, VarIntSupportMut};
 use bytes_varint::try_get_fixed::TryGetFixedSupport;
 
-
-//TODO unit test
 //TODO extract to crate?
-//TODO convenience for serializing / deserializing collections
 
 pub fn put_string(buf: &mut BytesMut, s: &str) {
     buf.put_usize_varint(s.len());
@@ -21,4 +18,78 @@ pub fn try_get_string(buf: &mut impl Buf) -> anyhow::Result<String> {
 
     let s = String::from_utf8(result)?;
     Ok(s)
+}
+
+#[cfg(test)]
+mod tests {
+    use bytes::{Buf, BytesMut};
+    use rstest::rstest;
+    use crate::util::buf::{put_string, try_get_string};
+
+    #[rstest]
+    #[case::empty("", vec![0])]
+    #[case::a("a", vec![1,97])]
+    #[case::abc("abc", vec![3,97,98,99])]
+    #[case::umlaut("ä", vec![2,0xc3,0xa4])]
+    #[case::heart("❤️", vec![6, 226,157,164,239,184,143])]
+    #[case::hearts("❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️", vec![
+        132,1, //NB: length is variable-length encoded
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+        226,157,164,239,184,143,
+    ])]
+    fn test_put_string(#[case] s: &str, #[case] expected: Vec<u8>) {
+        let mut buf = BytesMut::new();
+        put_string(&mut buf, s);
+        assert_eq!(&buf, &expected);
+
+        let mut deser_buf = &mut buf;
+        let deser = try_get_string(deser_buf).unwrap();
+        assert!(deser_buf.is_empty());
+        assert_eq!(&deser, s);
+    }
+
+    #[test]
+    fn test_try_get_string_remaining() {
+        let mut buf = BytesMut::from(b"\x01abc".as_slice());
+        let mut deser_buf = &mut buf;
+        let actual = try_get_string(&mut deser_buf).unwrap();
+        assert_eq!(&actual, "a");
+        assert_eq!(deser_buf.chunk(), b"bc");
+    }
+
+    #[test]
+    fn test_try_get_string_too_short() {
+        let mut buf = BytesMut::from(b"\x02a".as_slice());
+        let mut deser_buf = &mut buf;
+        let actual = try_get_string(&mut deser_buf);
+        assert!(actual.is_err());
+    }
+
+    #[test]
+    fn test_try_get_string_not_unicode() {
+        let mut buf = BytesMut::from(b"\x02\xc0\xaf".as_slice());
+        let mut deser_buf = &mut buf;
+        let actual = try_get_string(&mut deser_buf);
+        assert!(actual.is_err());
+    }
 }
